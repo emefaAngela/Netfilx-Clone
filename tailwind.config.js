b8e980bd882a45d79a4a9baacdbb43cb60065cdf
emefaAngela <63966516+emefaAngela@users.noreply.github.com>
2022-07-31 12:50:06 +0000

    importing font

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,10 +11,15 @@ module.exports = {
     extend: {
       colors:{
         red:'#E50914',
-        ash:'#95A5A6'
+        ash:'#95A5A6',
+        tile:'rgb(48,48,48)',
+        break:'#222222'
       },
       backgroundImage:{
         'hero-picture':"url('/images/netflix.jpg')"
+      },
+      fontFamily:{
+        body:["'Bebas Neue',cursive"]
       }
     },
   },
